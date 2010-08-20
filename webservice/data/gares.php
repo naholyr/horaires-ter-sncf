9c04da22053c26f202c886c9162a47109ee037c0
naholyr <naholyr@b04fef20-2e95-11df-a8e3-ab3d4dd5af44>
2010-08-20 15:07:44 +0000

    Nouveau système d'accès au listing des gares, avec support des mises à jour en fournissant la date de dernière maj en paramètre

---
+++ webservice/data/gares.php
@@ -0,0 +1,44 @@
+<?php
+
+include dirname(__FILE__).'/gares.config.php';
+
+$dsn = 'mysql:host=localhost;dbname=horairestersncf';
+$user = 'horairestersncf';
+$pass = 'CdBFcEaGj9jJJdJV';
+
+$sql_select = 'SELECT id, nom, region, adresse, latitude, longitude, updated_at FROM gares';
+$sql_drop = 'DROP TABLE IF EXISTS gares';
+$sql_create = 'CREATE TABLE gares (id INTEGER PRIMARY KEY AUTO_INCREMENT, nom VARCHAR(64) UNIQUE, region VARCHAR(32) NOT NULL, adresse VARCHAR(255) NOT NULL, latitude DOUBLE NOT NULL, longitude DOUBLE NOT NULL, updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP())';
+$sql_insert = 'INSERT INTO gares (nom, region, adresse, latitude, longitude) VALUES (:nom, :region, :adresse, :latitude, :longitude)';
+$sql_where = '';
+
+$db = new PDO($dsn, $user, $pass);
+$db->query('SET NAMES UTF8');
+
+if (array_key_exists('last_update', $_GET)) {
+  $ts = strtotime($_GET['last_update']);
+  if ($ts !== false) {
+    $sql_where = 'WHERE updated_at > ' . $db->quote(date('Y-m-d H:i:s', $ts), PDO::PARAM_STR);
+  }
+}
+
+if (!($stq = $db->query($sql_select.' '.$sql_where))) {
+  $db->query($sql_drop);
+  $db->query($sql_create);
+  $sti = $db->prepare($sql_insert, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+  $lines = file(dirname(__FILE__).'/gares.utf8.txt');
+  foreach ($lines as $line) {
+    list($nom, $region, $adresse, $latitude, $longitude) = array_map('trim', explode('#', trim($line)));
+    $sti->execute(array('nom' => $nom, 'region' => $region, 'adresse' => $adresse, 'latitude' => floatval($latitude), 'longitude' => floatval($longitude)));
+  }
+  $stq = $db->query($sql_select);
+}
+
+if ($stq) {
+  header('Content-Type: text/plain; charset=UTF-8');
+  echo $db->query('SELECT COUNT(*) FROM gares '.$sql_where)->fetchColumn(), "\n";
+  echo $db->query('SELECT MAX(updated_at) FROM gares')->fetchColumn(), "\n";
+  foreach ($stq as $row) {
+    echo implode('#', array($row['id'], $row['region'], $row['nom'], $row['adresse'], $row['latitude'], $row['longitude'])), "\n";
+  }
+}
